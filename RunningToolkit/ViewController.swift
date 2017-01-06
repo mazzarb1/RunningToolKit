759916372e7eccea05aff31282c17fa9b04043b6
Brandon Mazzarella <branmazz@optonline.net>
2017-01-06 11:57:18 -0500

    Initial Commit

---
+++ RunningToolkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunningToolkit
+//
+//  Created by Brandon Mazzarella on 1/6/17.
+//  Copyright © 2017 Brandon Mazzarella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
